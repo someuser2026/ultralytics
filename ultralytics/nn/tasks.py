7e7d96e39a881596877fe9afecdf0a344d52e220
someuser2026 <forgithub2026@gmail.com>
2025-08-26 20:48:51 +1000

    Add support for convnextV2

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1749,6 +1749,12 @@ def parse_model(d, ch, verbose=True):
             c2 = args[0]
             c1 = ch[f]
             args = [*args[1:]]
+        elif m in frozenset({ConvNeXtDownsample, ConvNeXtStem}):
+            c1, c2 = ch[f], args[0]
+            args = [c1, c2]
+        elif m in frozenset({ConvNeXtBlock}):
+            c1, c2 = ch[f], args[0]
+            args = [c1, c2, *args[1:]]
         else:
             c2 = ch[f]
 
