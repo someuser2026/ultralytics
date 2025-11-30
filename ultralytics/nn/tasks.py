7117e1ea5b579c32281e6b695068d03403389f48
someuser2026 <forgithub2026@gmail.com>
2025-11-30 21:34:00 +1100

    Fix bug for patch size and stride cal

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -452,7 +452,7 @@ class DetectionModel(BaseModel):
         m = self.model[-1]  # Detect()
         if isinstance(m, Detect):  # includes all Detect subclasses like Segment, Pose, OBB, YOLOEDetect, YOLOESegment
             s = deepcopy(self.yaml["stride"])  # 2x min stride
-            ps = int(deepcopy(self.yaml.get("patch_size", None)))  # pathc_size for transform based models
+            ps = deepcopy(self.yaml.get("patch_size", None))  # pathc_size for transform based models
             # s = self.yaml.pop("stride")
             # s = 256
             m.inplace = self.inplace
@@ -473,6 +473,7 @@ class DetectionModel(BaseModel):
             print("-"*50)
             self.stride = m.stride
             if ps:
+                ps = int(ps)
                 max_stride_pos = torch.argmax(self.stride)
                 max_stride = int(self.stride[max_stride_pos].item())
                 new_max_stride = int((max_stride * ps) // (math.gcd(max_stride, ps)))
