208ff4f25dec4d57cde7399c5d16b7f1d65c6e9a
someuser2026 <forgithub2026@gmail.com>
2026-02-03 00:50:46 +1100

    Bug Fix: Correct name parsing in wandb project

--- ultralytics/utils/callbacks/wb.py
+++ ultralytics/utils/callbacks/wb.py
@@ -356,7 +356,7 @@ def on_pretrain_routine_start(trainer):
         proj_name_parts = str(trainer.args.project).split("/")
         req_parts = [p for p in proj_name_parts if "imgsz" in p]
         req_parts.extend(trainer.args.task)
-        req_parts.extend(proj_name_parts[-1])
+        req_parts.extend([proj_name_parts[-1]])
         project_cleaned = "-".join(req_parts) if req_parts else "Ultralytics"
         project_cleaned = project_cleaned.replace("yolo", "")
         
